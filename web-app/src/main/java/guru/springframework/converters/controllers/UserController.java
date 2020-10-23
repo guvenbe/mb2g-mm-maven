c0940eaaeaed328f060b059729113361e7408e7b
Bora Guven <guvenbe@gmail.com>
2020-10-23 02:01:36 -0400

    aded mapping

--- web-app/src/main/java/guru/springframework/converters/controllers/UserController.java
+++ web-app/src/main/java/guru/springframework/converters/controllers/UserController.java
@@ -3,10 +3,11 @@ package guru.springframework.converters.controllers;
 
 import guru.springframework.entities.User;
 import guru.springframework.domain.UserCommand;
+import guru.springframework.converters.UserMapper;
 
 public class UserController {
 
     User saveUser(UserCommand command){
-        return  new User();
+        return  UserMapper.INSTANCE.userCommandToUser(command);
     }
 }
