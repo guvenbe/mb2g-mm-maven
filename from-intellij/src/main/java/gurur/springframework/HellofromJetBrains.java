13dad57b3455dab6de5ad6bcee84d50a402030bb
Bora Guven <guvenbe@gmail.com>
2020-10-22 23:31:41 -0400

    from intellij

---
+++ from-intellij/src/main/java/gurur/springframework/HellofromJetBrains.java
@@ -0,0 +1,9 @@
+package gurur.springframework;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class HellofromJetBrains {
+    public String sayHello(){
+        return "Hello from jet brains";
+    }
+}
